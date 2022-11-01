1b8b0630986d566a3c1c220a22308a6c97057e62
Jeff <jcgilbert70@gmail.com>
2022-11-01 10:48:54 -0500

    added some pseudo code to js

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+function init() {
+
+
+
+    renderHistory() // display any stored history when initialized
+}
+
+
+
+
+// store searched history in local storage
+function storedHistory() {
+
+}
+
+
+
+
+// display cities previously searched, pull from local storage
+function renderHistory() {
+
+
+
+// create new element to display each search history
+
+
+
+}
+
+
+
+
+
+
+
+// have function run on form submit
+function getWeather() {
+
+
+
+
+
+
+
+    storedHistory()
+    renderHistory()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init()
