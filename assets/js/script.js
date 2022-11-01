593ca96670b3180f713ba2e1caf27f542884faa3
Jeff <jcgilbert70@gmail.com>
2022-11-01 12:05:40 -0500

    added fetch, need to get a key still, will test after

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,38 @@
-function init() {
+var locationInputEl = $('#locationInput');
+var locationSearchedEl = $('#locationSearched');
+var apiKey = "";
 
+function init() {
+    console.log("initilize function started")
 
 
     renderHistory() // display any stored history when initialized
 }
 
 
+var inputSumbitHandler = function (event) {
+    event.preventDefault();
+    console.log("input submit handler function started")
+
+
+    var searchedLocation = locationInputEl.value().trim();
+    console.log("searched location is: " + searchedLocation);
+    if (searchedLocation) {
+        getWeather(searchedLocation);
+
+        locationSearchedEl.textContent = '';
+        locationInputEl.value = '';
+    } else {
+        alert('Please enter a location to search for weather forcast')
+    }
+};
+
 
 
 // store searched history in local storage
 function storedHistory() {
+    console.log("stored history function started")
+
 
 }
 
@@ -18,10 +41,11 @@ function storedHistory() {
 
 // display cities previously searched, pull from local storage
 function renderHistory() {
+    console.log("render history function started")
 
 
 
-// create new element to display each search history
+    // create new element to display each search history
 
 
 
@@ -34,40 +58,32 @@ function renderHistory() {
 
 
 // have function run on form submit
-function getWeather() {
+var getWeather = function (locationName) {
+    console.log("get weather function started")
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + locationName + "&appid=" + apiKey;
 
-
-
-
-
-
-
-    storedHistory()
-    renderHistory()
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displaySearch(data);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error){
+            alert('Unable to connect to OpenWeatherMap')
+        })
 }
 
 
 
+$('#submitBtn').on('click', function () {
+    inputSumbitHandler();
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// will need button click function for created location history buttons
 
 init()
+
